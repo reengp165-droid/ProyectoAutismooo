2f50600a19ee295b6285d36b1604e8e2205b4020
reengp165-droid <reengp165@igmail.com>
2025-12-04 18:37:17 -0600

    Primer proyecto

---
+++ src/Bienvenida.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Bienvenida() {
+  return (
+    <div>
+      <h1>loool</h1>
+    </div>
+  )
+}
+
+export default Bienvenida
---
+++ src/INICIOO.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function INICIOO() {
+  return (
+    <div>
+      <h1 className='titulo'> Conecta Azul</h1>
+      <h2 className='subTitulo'> ¡Aprende con nosotros! </h2>
+
+      <button >Iniciar</button>
+    </div>
+  )
+}
+
+export default INICIOO
